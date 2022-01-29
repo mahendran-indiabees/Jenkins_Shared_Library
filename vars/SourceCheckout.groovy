0f7a5545f74bce4f750829150b303f7bd21787ed
mahendran-indiabees <mahivalarmathi1@gmail.com>
2022-01-29 10:55:14 +0530

    echo command added

--- vars/SourceCheckout.groovy
+++ vars/SourceCheckout.groovy
@@ -1,5 +1,9 @@
 def call (def giturl, def branch) {
-
+	echo ""
+	echo "********** Git URL:  ${giturl} **********"
+	echo ""
+	echo "********** Git Branch:  ${branch} **********"
+	echo ""
 	if(giturl.contains("github.com"))
 	{
 	checkout([$class: 'GitSCM', branches: [[name: branch ]], extensions: [], userRemoteConfigs: [
