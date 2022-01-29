438627a5286001139fab37a0cc9db6d6622ae3e1
mahendran-indiabees <mahivalarmathi1@gmail.com>
2022-01-29 15:47:52 +0530

    test

--- vars/UploadArtifacts.groovy
+++ vars/UploadArtifacts.groovy
@@ -9,7 +9,7 @@ def call (def filetype,def filename,def artifact_id,def group_id,def Nexus_cred_
 		nexusVersion: 'nexus3', 
 		protocol: 'http', 
 		repository: repositoryName, 
-		version: "${BuildNumber}"-SNAPSHOT
+		version: BuildNumber
 	}
 	catch(Exception th) {
 		echo "Artifact upload failed. Please Check console log"
